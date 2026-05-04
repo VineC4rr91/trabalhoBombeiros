0e7ad1322d8a085e613a4ec49e1915d2284b914c
aluno <aluno@lab01-06>
2026-05-04 16:42:10 -0300

    iiiiiiiHAAAAAA

---
+++ Bombeiros/src/bombeiros/Bombeiro.java
@@ -0,0 +1,17 @@
+
+package bombeiros;
+
+/**
+ *
+ * @author aluno
+ */
+public class Bombeiro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
