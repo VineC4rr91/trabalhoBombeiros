1465c30fbf219fa143cd21f9f874e7f6bbc60b0c
aluno <aluno@lab01-06>
2026-05-11 16:45:37 -0300

    Horarh

---
+++ gestaodepessoas/src/main/java/Ocorrencias.java
@@ -0,0 +1,11 @@
+/*
+    Feito por Vine e Txuli
+ */
+public class Ocorrencias {
+    private String tipo;
+    private Date data;
+    private String hora;
+    private String status;
+}
+public void consultar(){
+}
